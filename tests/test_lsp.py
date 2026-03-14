8ef6b27e4a06f4ef8abef35543e4db49fdb253f1
Sean Evans <sean.w.evans@gmail.com>
2026-03-14 00:13:37 -0400

    Use compiler entities for LSP symbol resolution

--- tests/test_lsp.py
+++ tests/test_lsp.py
@@ -2,6 +2,7 @@ from lockstep_compiler.lsp import (
     build_struct_member_index,
     find_member_definition,
     provide_bind_completion_items,
+    provide_hover_info,
 )
 
 
@@ -80,3 +81,69 @@ pipeline P {
         "Bind route template",
         "Shader/filter callable",
     ]
+
+
+TRICKY_FORMAT_SOURCE = """
+struct Particle {
+    float mass; // mass comment
+    /* block comment before velocity */
+    Vec3 velocity;
+};
+
+shader
+Apply(
+    in Particle src,
+    out Particle dst,
+    uniform float dt
+) {
+    Particle local = src;
+    dst.velocity = local.velocity;
+    float speed = dt;
+}
+
+// comment before filter
+filter
+Post(
+    in Particle src,
+    out Particle dst
+) {
+    dst = src;
+}
+
+pure float
+blend(float a, float b)
+{
+    return a;
+}
+"""
+
+
+def test_build_struct_member_index_ignores_comments_and_tracks_locations():
+    index = build_struct_member_index(TRICKY_FORMAT_SOURCE)
+
+    assert index["Particle"]["mass"].line == 2
+    assert index["Particle"]["velocity"].line == 4
+
+
+def test_infer_and_definition_resolve_member_declared_from_body_ast():
+    line = 14
+    column = 8  # dst.velocity
+
+    definition = find_member_definition(TRICKY_FORMAT_SOURCE, line, column)
+
+    assert definition is not None
+    assert definition.struct_name == "Particle"
+    assert definition.field_name == "velocity"
+    assert definition.line == 4
+
+
+def test_hover_resolves_variable_and_callable_symbols_with_tricky_formatting():
+    variable_hover = provide_hover_info(TRICKY_FORMAT_SOURCE, 14, 20)  # local.velocity
+    shader_hover = provide_hover_info(TRICKY_FORMAT_SOURCE, 8, 0)  # Apply
+    filter_hover = provide_hover_info(TRICKY_FORMAT_SOURCE, 20, 0)  # Post
+    pure_hover = provide_hover_info(TRICKY_FORMAT_SOURCE, 28, 0)  # blend
+
+    assert variable_hover == "(field) `Particle.velocity: Vec3`"
+    assert shader_hover == "(shader) `shader Apply(...)`"
+    assert filter_hover == "(filter) `filter Post(...)`"
+    assert pure_hover == "(pure) `pure blend(...)`"
